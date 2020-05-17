d1281f9ea3ab812cd25899b4f2f958b37c818d16
thebubbleblower <carmenwen1992@gmail.com>
2020-05-17 15:50:45 -0400

    text shadow

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,35 +7,35 @@ class Nav extends React.Component {
     super(props);
     this.state = {
       show: false,
+      status: 'not-selected'
     }
     this.showResume = this.showResume.bind(this);
     this.hideResume = this.hideResume.bind(this);
   }
 
   showResume() {
-    this.setState({show: true});
-    console.log(this)
+    this.setState({show: true, status: 'selected'});
   }
 
   hideResume() {
-    this.setState({show: false});
+    this.setState({show: false, status: 'not-selected'});
   }
 
   render() {
     return <div className="nav">
-      <button onClick={this.showResume}>
+      <button onClick={this.showResume} className={this.state.status}>
         Resume
       </button>
-      <button>
+      {/* <button>
         About
-      </button>
+      </button> */}
       <Popup open={this.state.show} 
             closeOnDocumentClick onClose={this.hideResume} 
-            contentStyle={{border:"none", padding:"none", background:"none"}}
+            contentStyle={{border:"none", padding:"none", background:"none", width:"50px"}}
             >
-            <a href="../../public/assets/CarmenWen_Resume_(Official).pdf" target="blank">
+            {/* <a href="../../public/assets/CarmenWen_Resume_(Official).pdf" target="blank"> */}
               <Resume />
-            </a>
+            {/* </a> */}
       </Popup>
     </div>
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,29 +15,32 @@ class Projects extends Component {
           Projects
         </h2>
       </div>
-      <div className="projects">
-        <div>
-          <h4>Dot-Eater</h4>
-          <div className="project-image" id="doteater">
-            <img className="static" src="../../public/assets/pacman.png"/>
-            <img className="moving" src="../../public/assets/doteater2.gif"/>
+      <div className="all-projects">
+        <div className="project">
+          <h4>WEATHER PET</h4>
+          <div className="project-image" id="weatherpet">
+            <img className="moving" src="../../public/assets/weatherpet.gif"/>
+            <img className="static" src="../../public/assets/weatherpetrain.png"/>
           </div>
+          <div className="short-description">JAVASCRIPT WEATHER ILLUSTRATION</div>
+        </div>
+        <div className="project">
+          <h4>DOT-EATER</h4>
+          <div className="project-image" id="doteater">
+            <img className="moving" src="../../public/assets/doteater2.gif"/>
+            <img className="static" src="../../public/assets/pacman.png"/>
+          </div>
+          <div className="short-description">REALTIME MULTIPLAYER ONLINE VERSION OF PAC-MAN BATTLE ROYALE</div>
           {/* <button><a href="http://dot-eater.herokuapp.com/" target="_blank">Live Version</a></button>
           <button><a href="https://github.com/Team-Blade/Capstone-Project" target="_blank">View Code on Github</a></button> */}
         </div>
-        <div>
-          <h4>Weather Pet</h4>
-          <div className="project-image" id="weatherpet">
-            <img className="static" src="../../public/assets/weatherpetrain.png"/>
-            <img className="moving" src="../../public/assets/weatherpet.gif"/>
-          </div>
-        </div>
-        <div>
-          <h4>Twilight Gems Shop</h4>
+        <div className="project">
+          <h4>TWILIGHT GEMS SHOP</h4>
           <div className="project-image" id="shop">
-            <img className="static" src="../../public/assets/ecommerce.png"/>
             <img className="moving" src="../../public/assets/shop.gif"/>
+            <img className="static" src="../../public/assets/ecommerce.png"/>
           </div>
+          <div className="short-description">SCALABLE E-COMMERCE SITE</div>
         </div>
       </div>
     </div>
