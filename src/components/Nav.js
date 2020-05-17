ca980399494b1d2eb6784be85f83381fced82a33
thebubbleblower <carmenwen1992@gmail.com>
2020-05-17 12:07:06 -0400

    popup resume pdf

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import Resume from "./Resume.js"
 
-const Nav = () => {
+class Nav extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show: false,
+    }
+    this.showResume = this.showResume.bind(this);
+    this.hideResume = this.hideResume.bind(this);
+  }
+
+  showResume() {
+    this.setState({show: true});
+    console.log(this)
+  }
+
+  hideResume() {
+    this.setState({show: false});
+  }
+
+  render() {
     return <div className="nav">
-        <button>
-            <a href="">Resume</a>
-        </button>
-        <button>
-            <a href="">About</a>
-        </button>
+      <button onClick={this.showResume}>
+        Resume
+      </button>
+      <button>
+        About
+      </button>
+      <Popup open={this.state.show} 
+            closeOnDocumentClick onClose={this.hideResume} 
+            contentStyle={{border:"none", padding:"none", background:"none"}}
+            >
+        <Resume />
+      </Popup>
     </div>
+  }
 }
 
 export default Nav;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import { Document,Page } from 'react-pdf/dist/entry.webpack'
 const Resume = () => {
     return <div id="resumePDF">
         <Document file="../../public/assets/CarmenWen_Resume_(Official).pdf">
-            <Page pageNumber={1}/>
+            <Page pageNumber={1} height={window.innerHeight * 0.96}/>
         </Document>
     </div>
 }
